77f64f999f8a7f6698953b87f149d723e559d30c
JinXiao <110443900+1793925850@users.noreply.github.com>
2023-05-23 16:26:47 +0800

    add

--- Vue3_Learning/demo/src/App.vue
+++ Vue3_Learning/demo/src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <HeaderVue />
+  <MainVue />
 </template>
 
 <script>
 import HeaderVue from "@/pager/HeaderVue.vue";
+import MainVue from "@/pager/MainVue.vue";
 export default {
     components:{
-        HeaderVue
+        HeaderVue,
+        MainVue
     }
 }
 </script>
---
+++ Vue3_Learning/demo/src/pager/MainVue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="main">
+      <h3>MainVue</h3>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.main{
+    float: left;
+    width: 70%;
+    height: 600px;
+    border: 5px solid #999;
+    box-sizing: border-box;
+}
+</style>
